302c2b179423b87c61c13e7fba3688e2651487d0
ducnv82 <ducnv82@gmail.com>
2014-01-19 14:45:24 +0700

    Add comments to README and fix compilation error in test class

---
+++ src/main/java/generic/service/UserService.java
@@ -0,0 +1,10 @@
+package generic.service;
+
+import generic.domain.User;
+
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserService extends GenericService<User> {
+
+}
